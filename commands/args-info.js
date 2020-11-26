bec4430865844bb6cf07f761e5e1646a931b1319
blueca <nickoldacre@outlook.com>
2020-11-26 20:32:34 +0000

    add args property

--- commands/args-info.js
+++ commands/args-info.js
@@ -1,13 +1,12 @@
 module.exports = {
   name: 'args-info',
   description: 'info about args',
+  args: true,
   execute(msg, args) {
-    if (!args.length) {
-      return msg.channel.send('no arguments provided');
-    } else if (args[0] === 'foo') {
+    if (args[0] === 'foo') {
       return msg.channel.send('bar');
     }
 
-    msg.channel.send(`First arg: ${args[0]}`);
+    msg.channel.send(`arguments: ${args}\nArguments length: ${args.length}`);
   },
 };

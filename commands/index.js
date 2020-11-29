dbd2e065543ba7aa99555c57bcf28aea60a008a2
blueca <nickoldacre@outlook.com>
2020-11-29 14:10:24 +0000

    remove initial commands

--- commands/index.js
+++ commands/index.js
@@ -1,21 +1,11 @@
-const test = require('./test');
-const server = require('./server');
-const userInfo = require('./user-info');
-const argsInfo = require('./args-info');
 const avatar = require('./avatar');
-const kick = require('./kick');
 const prune = require('./prune');
 const help = require('./help');
 const reload = require('./reload');
 const youtube = require('./youtube');
 
 module.exports = {
-  test,
-  server,
-  'user-info': userInfo,
-  'args-info': argsInfo,
   avatar,
-  kick,
   prune,
   help,
   reload,
